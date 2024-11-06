a5bd85f999514fcb18474b0d3e96819023163f30
Ayush Sharma <kshitij3160@gmail.com>
2024-11-06 23:58:04 +0530

    setup cors in backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  
+  if(!configService.isProduction()){
+    app.enableCors();
+  }
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
